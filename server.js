33319f8b2fcee3e98eb283a2250abe4160478319
Richard Sabbarton <richard.sabbarton@vonage.com>
2023-07-12 12:48:20 +0000

    First CMD line update

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+
+console.log('Neru Instance')
+const neru = require('neru-alpha').neru
+console.log('getting neru state')
+const state = neru.getInstanceState()
+
 
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Express and Node are working Fine')
+  res.redirect('/app.html')
 })
 
+
+app.get('/_/health', async (req, res) => {
+  res.sendStatus(200);
+});
+
+const port = process.env.NERU_APP_PORT || process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Application Listing ON Port: ${port}`)
 })
