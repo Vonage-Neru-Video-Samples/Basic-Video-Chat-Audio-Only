dc707e0d72ad5acfaf7778e0a7c2b90b74bade79
Richard Sabbarton <richard.sabbarton@vonage.com>
2024-05-09 12:33:30 +0000

    Added device permissions testing

--- public/video/Basic-Video-Chat-Audio-Only/js/app.js
+++ public/video/Basic-Video-Chat-Audio-Only/js/app.js
@@ -61,28 +61,73 @@ function initializeSession() {
   session.on("connectionDestroyed", (event)=>{ console.log(event.type, event.connection.id)})
   
   let videoSource = null
-  // initialize the publisher
-  const publisherOptions = {
-    "insertMode": "append",
-    "width": "100%",
-    "height": "100%",
-    "name": "{\"role\":\"provider\",\"platform\":\"web\",\"designation_desc\":\"Provider\",\"role_id\":1,\"role_name\":\"Vyas, Dr. Meet\",\"joined_timestamp\":\"2024-05-08T12:42:39.894Z\"}",
-    "videoSource": videoSource, //it will be null if no video source
-    "publishVideo": videoSource ? true : false,
-    "capableSimulcastScreenshare": false
-  };
-
-  const publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+  let audioSource = null
+  let hasAudio = false
+  let hasVideo = false
 
   // Connect to the session
-  session.connect(token, (error) => {
-    if (error) {
-      handleError(error);
-    } else {
-      // If the connection is successful, publish the publisher to the session
-      session.publish(publisher, handleError);
+  navigator.mediaDevices.getUserMedia({video: false, audio: true})
+  .then(mediaStream=>{
+    if(mediaStream.getAudioTracks().length > 0) {
+      hasAudio = true
+      audioSource = mediaStream.getAudioTracks()[0]
     }
-  });
+  })
+  .then(async ()=>{
+    await navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then(mediaStream=>{
+      let hasAudio = false
+      let hasVideo = false
+      if(mediaStream.getVideoTracks().length > 0) {
+        hasVideo = true
+        videoSource = mediaStream.getVideoTracks()[0]
+        console.log(hasVideo, videoSource)
+      }
+      if(mediaStream.getAudioTracks().length > 0) {
+        hasAudio = true
+        audioSource = mediaStream.getAudioTracks()[0]
+        console.log(hasAudio, audioSource)
+      }
+    })
+    .catch((error)=>console.log(error, 'no video but audio OK'))
+    
+  })
+  .catch((error)=>console.log(error, "No audio so cannot proceed!"))
+  .finally(()=>{
+    // initialize the publisher
+    const publisherOptions = {
+      "insertMode": "append",
+      "width": "100%",
+      "height": "100%",
+      "name": "{\"role\":\"provider\",\"platform\":\"web\",\"designation_desc\":\"Provider\",\"role_id\":1,\"role_name\":\"Vyas, Dr. Meet\",\"joined_timestamp\":\"2024-05-08T12:42:39.894Z\"}",
+      "videoSource": videoSource, //it will be null if no video source
+      "publishVideo": videoSource ? true : false,
+      "audioSource": audioSource, //it will be null if no video source
+      "publishAudio": audioSource ? true : false,
+      "capableSimulcastScreenshare": false
+    }
+
+    console.log(publisherOptions)
+
+    const publisher = OT.initPublisher('publisher', publisherOptions, async (err) => {
+      if (err) {
+        console.error('Publisher creation error: ', err);
+        reject(err)
+      } else {
+        //audioSource ? publisher.setAudioSource(this.audioSource) : ''
+        //videoSource ? publisher.setVideoSource(videoSource) : ''
+      }
+    });
+  
+    session.connect(token, (error) => {
+      if (error) {
+        handleError(error)
+      } else {
+        // If the connection is successful, publish the publisher to the session
+        session.publish(publisher, handleError)
+      }
+    })
+  })
 }
 
 
